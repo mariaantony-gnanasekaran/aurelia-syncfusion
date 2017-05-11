71f396d97c328673b159a9d8914e853874ef16ab
mariaantony-gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2017-05-11 09:59:13 +0530

    Update 2017-05-11T04:27:29.951Z

--- src/themes/index.js
+++ src/themes/index.js
@@ -4,20 +4,22 @@ export class Index {
         this.themeInfo = {
             "Flat Azure": { "path": "default-theme", "theme": "flatlight" }, "Flat Azure Dark": { "path": "flat-azure-dark", "theme": "flatdark" }, "Flat Lime": { "path": "flat-lime", "theme": "flatlight" }, "Flat Lime Dark": { "path": "flat-lime-dark", "theme": "flatdark" }, "Flat Saffron": { "path": "flat-saffron", "theme": "flatlight" },
             "Flat Saffron Dark": { "path": "flat-saffron-dark", "theme": "flatdark" }, "Gradient Azure": { "path": "gradient-azure", "theme": "gradientlight" }, "Gradient Azure Dark": { "path": "gradient-azure-dark", "theme": "gradientdark" }, "Gradient Lime": { "path": "gradient-lime", "theme": "gradientlight" }, "Gradien Lime Dark": { "path": "gradient-lime-dark", "theme": "gradientdark" },
-            "Gradient Saffron": { "path": "gradient-saffron", "theme": "gradientlight" }, "Gradient Saffron Dark": { "path": "gradient-saffron-dark", "theme": "gradientdark" }, "Bootstrap": { "path": "bootstrap-theme", "theme": "bootstrap" }, "High contrast 1": { "path": "high-contrast-01", "theme": "high-contrast-01" }, "High contrast 2": { "path": "high-contrast-02", "theme": "high-contrast-01" }, "Material": { "path": "material", "theme": "material" }, "Office 365": { "path": "office-365", "theme": "material" }};
+            "Gradient Saffron": { "path": "gradient-saffron", "theme": "gradientlight" }, "Gradient Saffron Dark": { "path": "gradient-saffron-dark", "theme": "gradientdark" }, "Bootstrap": { "path": "bootstrap-theme", "theme": "bootstrap" }, "High contrast 1": { "path": "high-contrast-01", "theme": "high-contrast-01" }, "High contrast 2": { "path": "high-contrast-02", "theme": "high-contrast-01" }, "Material": { "path": "material", "theme": "material" }, "Office 365": { "path": "office-365", "theme": "material" }
+        };
         this.name = ["Flat Azure", "Flat Azure Dark", "Flat Lime", "Flat Lime Dark", "Flat Saffron", "Flat Saffron Dark", "Gradient Azure", "Gradient Azure Dark", "Gradient Lime", "Gradien Lime Dark", "Gradient Saffron", "Gradient Saffron Dark", "Bootstrap", "Office 365", "High contrast 1", "High contrast 2", "Material"];//office 365 is currently in development
     }
     click(theme) {
-        $("body").fadeOut(0);
-        $("body").fadeIn(1300);
+        jQuery('body').fadeOut(400, () => {
+            this.updateTheme(theme)
+                .then(() => jQuery('body').fadeIn());
+        });
+    }
+
+    updateTheme(theme) {
         window.themeName = theme;
         window.theme = this.themeInfo[theme].theme;
         let path = this.themeInfo[theme].path;
-        this.removeCss();
-        this.updateCss();
-        this.themePath(path);
-        this.commonPath();
-        this.datavisualizationTheme();
+        return Promise.all([this.removeCss(), this.updateCss(), this.themePath(path),this.commonPath(),this.datavisualizationTheme()]);
     }
 
     datavisualizationTheme() {
